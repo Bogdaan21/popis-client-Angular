8d90952d63af3c4b60b5a36a6f76da0c169070b8
Bogdaan21 <gajevicbogdan@yahoo.com>
2022-06-30 13:20:54 +0200

    registruj

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent }, // path:'home' (dodatak u url /home), HomeComponent (gdje ga preusmjeravas kad udjes na URL)
+];
+
+//PROMJENA PORTA U ANGULAR, ng serve --port=(zeljeni port) 
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
